1d60412d77c2242a386e22ef4f90b95ce44cf097
rafaelcorreia <rafaelmdcorreia@gmail.com>
2025-11-16 11:34:47 +0000

    first commit

--- exercises/02_basic_calculator/03_if_else/src/lib.rs
+++ exercises/02_basic_calculator/03_if_else/src/lib.rs
@@ -2,7 +2,14 @@
 /// `13` if `n` is divisible by `3`,
 /// `17` otherwise.
 fn magic_number(n: u32) -> u32 {
-    todo!()
+    let test: u32 = if n % 2 == 0 {
+        12
+    } else if n % 3 == 0 {
+        13        
+    } else {
+        17
+    };
+    test
 }
 
 #[cfg(test)]
